5968b7b68d243acdb4f64551988f7309973d9e23
Adrian Stabiszewski <as@grundid.de>
2017-02-02 19:08:56 +0100

    minor config, meetup setup

---
+++ pages/meetupScene.vue
@@ -0,0 +1,37 @@
+<template>
+    <section class="container">
+        Aktuelle Meetup Termine
+        <p>{{event.name}}</p>
+        <b></b>
+
+    </section>
+</template>
+<script>
+    import axios from 'axios'
+
+    var toggleEvents = function () {
+
+    };
+
+    export default {
+        data ({req}) {
+            return axios.get('https://api.meetup.com/codeforhn/events?&sign=true&photo-host=public&page=5&status=upcoming')
+                .then((res) => {
+                    console.log(res.data);
+                    return {
+                        event: res.data[0],
+                        events: res.data
+                    }
+                })
+        },
+        head () {
+            return {
+                title: 'Aktuelle Termine'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
+
