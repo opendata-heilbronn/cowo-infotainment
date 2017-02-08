9db104ecb5e1866b6df9a6c1d69f65ed6faf03f1
LeoDJ <leandro.spaeth@gmail.com>
2017-02-08 15:04:13 +0100

    start train information & add material design lite

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,12 @@ module.exports = {
       { hid: 'description', content: "Nuxt.js project" }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons'},
+      { rel: 'stylesheet', href: 'https://code.getmdl.io/1.3.0/material.indigo-pink.min.css'}
+    ],
+    script: [
+        {src: 'https://code.getmdl.io/1.3.0/material.min.js'}
     ]
   },
   /*
---
+++ pages/busBahn.vue
@@ -0,0 +1,29 @@
+<template>
+    <section class="container">
+        <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+            <header class="mdl-layout__header">
+                <div class="mdl-layout__header-row">
+                    <span class="mdl-layout-title">Bus- und Bahnverbindungen</span>
+                </div>
+            </header>
+        </div>
+        <p>
+            Hello World!
+        </p>
+    </section>
+</template>
+<script>
+    export default {
+        data() {
+
+        },
+        head () {
+            return {
+                title: "Bus- und Bahnverbindungen"
+            }
+        }
+    };
+</script>
+
+<style scoped>
+</style>
